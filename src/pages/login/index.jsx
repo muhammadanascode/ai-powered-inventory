d1a033129476e708e0448a0ea9381159572ce91d
muhammadanascode <anassohal34343@gmail.com>
2025-05-30 15:51:15 +0500

    Created login path

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Login = () => {
+
+    return (
+        <div className="login-page">
+            <h1>Login</h1>
+            <form>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" required />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
