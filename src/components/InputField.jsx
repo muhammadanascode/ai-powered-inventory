f9470e3ce01a84aea0cc0a3629175f1aef8339cd
muhammadanascode <anassohal34343@gmail.com>
2025-05-30 17:01:21 +0500

    Added Place holder

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import '../styles/inputField.css'
 
-const InputField = ({ label, name, type, value, onChange }) => (
-  <div className="auth-field">
+const InputField = ({ label, name, type, value, onChange, placeholder }) => (
+  <div className="input-field-div">
     <label htmlFor={name}>{label}</label>
     <input
+    className='input-field'
       id={name}
       name={name}
       type={type}
       value={value}
       onChange={onChange}
+      placeholder={placeholder}
       required
     />
   </div>
