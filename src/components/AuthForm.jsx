12bd2205de5c27a3b698ce0144461b25dd205c7e
muhammadanascode <anassohal34343@gmail.com>
2025-05-30 17:00:43 +0500

    added subtitle and link to navigate

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
-// import '../styles/auth.css';
+import '../styles/auth.css';
 
-const AuthForm = ({title,submitLabel,children,onSubmit}) => {
-  return (
-    <>
-    <div className="auth-container">
-        <h2 className="auth-title">{title}</h2>
-        {children}
-        <button type="submit" onClick={onSubmit}>{submitLabel}</button>
-    </div>
-    </>
-  );
+const AuthForm = ({ title, submitLabel, children, onSubmit, subtitle }) => {
+    return (
+        <>
+            <div className="auth-container">
+                <h2 className="auth-title">{title}</h2>
+                <h3 className="auth-subtitle">{subtitle}</h3>
+                {children}
+                {submitLabel.toLowerCase() === 'signup' && (
+                    <p className="auth-link">
+                        Already have an account? <a href="/login" className='link'>Login</a>
+                    </p>
+                )}
+                {submitLabel.toLowerCase() === 'login' && (
+                    <p className="auth-link">
+                        Don't have an account? <a href="/signup" className='link'>Signup</a>
+                    </p>
+                )}
+                <button className='btn' type="submit" onClick={onSubmit}>{submitLabel}</button>
+            </div>
+        </>
+    );
 };
 
 export default AuthForm;
