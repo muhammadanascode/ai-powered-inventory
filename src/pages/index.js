9ea70f6519553add47433a9b1c793edaca6105c0
muhammadanascode <anassohal34343@gmail.com>
2025-06-10 20:45:00 +0500

    Removed login checks we have done this in layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,7 @@
-import NavBar from "@/components/NavBar";
-import SideBar from "@/components/SideBar";
 import styles from "@/styles/Home.module.css";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+
 
 export default function Home() {
-
-  const router = useRouter();
-  const [isVerifying, setIsVerifying] = useState(true);
-
-  useEffect(() => {
-    async function checkToken() {
-      const token = localStorage.getItem('authToken');
-      if (!token) {
-        router.push('/login');
-      }
-
-      const response = await fetch('/api/account/verifyToken', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: token,
-        },
-      });
-
-      const data = await response.json();
-
-      if (!data.valid) {
-        router.push('/login');
-      } else {
-        setIsVerifying(false);
-      }
-    }
-
-    checkToken();
-  }, []);
-
-  if (isVerifying) {
-    return <div>Verifying...</div>;
-  }
-
-
   return (
     <>
     hello
