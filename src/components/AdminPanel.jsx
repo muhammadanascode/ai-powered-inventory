8fa4320de47520721ca9239ac4f9990a188d70f0
muhammadanascode <anassohal34343@gmail.com>
2025-06-10 21:46:07 +0500

    Created admin panel for index page

---
+++ src/components/AdminPanel.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/AdminPanel.css"; 
+
+const AdminPanel = () => {
+    return (
+        <div className="admin-container">
+            <h1>Inventory Panel</h1>
+            {/* Your admin content here */}
+            <div>
+            </div>
+        </div>
+    )
+}
+
+export default AdminPanel;
