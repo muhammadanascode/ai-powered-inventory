8e2e8ec25ac2caecee41c4ad919bc8218d073686
muhammadanascode <anassohal34343@gmail.com>
2025-06-23 16:10:44 +0500

    retrieving account or sub accoutn id from req.user instead of query

--- src/pages/api/suppliers/delete/index.js
+++ src/pages/api/suppliers/delete/index.js
@@ -13,7 +13,8 @@ export default async function handler(req, res) {
     }
 
     authenticate(req, res, async () => {
-        let { account_id, sub_account_id, supplier_id } = req.query;
+        const { account_id, sub_account_id, } = req.user;
+        const { supplier_id } = req.query
 
         // Validate account_id
         if (!account_id && !sub_account_id) {
@@ -29,22 +30,11 @@ export default async function handler(req, res) {
         try {
 
             // authorizing user
-            if (account_id) {
-                if (req.user.account_id !== Number(account_id)) {
-                    return res.status(403).json({ error: "Forbidden: You can only delete suppliers from your own account" });
-                }
-            } else {
-                if (req.user.sub_account_id !== Number(sub_account_id)) {
-                    return res.status(403).json({ error: "Forbidden: You can only delete suppliers from your own sub-account" });
-                }
-                else if (req.user.account_type !== "manager") {
+            if (sub_account_id) {
+                if (req.user.account_type !== "manager") {
                     return res.status(403).json({ error: "Only owner and manager can delete suppliers" })
                 }
             }
-
-            //assigning of in case it is null
-
-            account_id = req.user.account_id;
 
             connection = await db.getConnection();
 
