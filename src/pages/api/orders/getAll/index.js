01154c77df2e5bedf11a10759ddcd107c264ed7b
muhammadanascode <anassohal34343@gmail.com>
2025-06-30 15:41:32 +0500

    Removed un necessary statements

--- src/pages/api/orders/getAll/index.js
+++ src/pages/api/orders/getAll/index.js
@@ -19,8 +19,6 @@ export default async function handler(req, res) {
     // Ensure authentication middleware runs before executing main logic
     authenticate(req, res, async () => {
         let { account_id, sub_account_id } = req.user;
-        const { order_status, price, created_at } = req.body;
-
 
         // Validate that at least one ID is provided
         if (!account_id && !sub_account_id) {
